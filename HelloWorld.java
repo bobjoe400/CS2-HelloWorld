99a18eca882a906698c87095ef7bb11d34ee824c
bobjoe400 <cnamattern@gmail.com>
2015-05-06 08:54:46 -0700

    Added some Scanner input. Removed some printLines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class HelloWorld
 {
+  private static Scanner in = new Scanner(System.in);
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
-    System.out.println("Goodbye");
-    System.out.println("Do another thing");
+    System.out.println("What is your quest");
+    String s = in.nextLine();
+    System.out.println(s +"! That sounds like fun.");
+    System.out.println("Goodbye.");
   }
 }
